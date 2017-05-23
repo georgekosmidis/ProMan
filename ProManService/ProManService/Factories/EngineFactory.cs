01a71c534488dd737016c1dbed994cd29ea5a051
Giorgos Kosmidis <g.kosmidis@epsilonet.gr>
2017-05-23 18:46:23 +0300

    gitignore updated

---
+++ ProManService/ProManService/Factories/EngineFactory.cs
@@ -0,0 +1,29 @@
+﻿using ProManService.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProManService.Factories
+{
+    public class EngineFactory
+    {
+        public EngineFactory()
+        {
+
+        }
+
+        public Engine Get()
+        {
+            return new Engine(
+                       new Engines.SVN(
+                           new EfData()
+                       ),
+                       new Engines.TFS(
+                           new EfData()
+                       )
+            );
+        }
+    }
+}
